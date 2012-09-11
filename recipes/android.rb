76343a5ac2988f9682bfa4d8938e719f98ed7041
ian douglas <id@id-sputnik.(none)>
2012-09-11 02:40:42 -0700

    Added Google App Engine for Python and Java; updated Android to latest download version

--- recipes/android.rb
+++ recipes/android.rb
@@ -1,9 +1,9 @@
-remote_file '/tmp/android-sdk_r20-linux.tgz' do
-  source 'http://dl.google.com/android/android-sdk_r20-linux.tgz'
-  checksum '58c0728fe75fd0ef6800bf70d3d030c16364272d5c018ccef38118e6a84cffdb'
+remote_file '/tmp/android-sdk_r20.0.3-linux.tgz' do
+  source 'http://dl.google.com/android/android-sdk_r20.0.3-linux.tgz'
+  checksum '919ea464007c2c4d04d5b366b4f64e916d99995e'
 end
 
-execute 'tar xvfz /tmp/android-sdk_r20-linux.tgz' do
+execute 'tar xvfz /tmp/android-sdk_r20.0.3-linux.tgz' do
   cwd '/usr/local'
   not_if {::File.exists? '/usr/local/android-sdk-linux'}
 end
